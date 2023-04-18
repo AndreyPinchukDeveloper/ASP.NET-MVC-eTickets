4df1706902850b25d9bde939d9204284f6b188a4
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-18 23:47:44 +0700

    dev Update
    - added IActorsService, ActorService and registered it into Program.cs(builder.services.addScope);
    - added create new actor view.

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAllActors();
+        Actor GetByID(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
