29fe4779fa0f45ed9c2e35e5afcce35f528dee58
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-22 23:04:34 +0700

    dev Update
    - added base entity repository class for base functionality, it provides us won't do the same big part including a lot of code for Cinemas, Producers etc.

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllActorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllActorsAsync();
+        Task<T> GetByIDAsync(int id);
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task RemoveAsync(int id);
+    }
+}
