d595d01afc11ce283e96e0eed7c41a4da12b91a8
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-07-11 21:35:54 +0700

    dev Update
    - added styles for xml table;
    - added view model class;
    - added reference to _Layout.

---
+++ eTickets/Models/XmlHandler/ViewModels/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Models.XmlHandler.ViewModels
+{
+    public class MovieViewModel
+    {
+        public MovieViewModel()
+        {
+            MovieGenre = new List<MovieGenre>();
+        }
+        public List<MovieGenre> MovieGenre { get; set;}
+
+        public int SumOfFailure { get; set;}  
+    }
+}
