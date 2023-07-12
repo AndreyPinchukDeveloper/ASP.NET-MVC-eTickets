e21c38f09fcd60bef8f4f0f30ecd70891b05806c
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-07-12 23:24:19 +0700

    dev Update
    - added home controller;
    - added cshtml View for import/export xml file.

---
+++ eTickets/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class HomeController:Controller
+    {
+        //get Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
